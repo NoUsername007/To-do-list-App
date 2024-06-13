f6bccd032ca26dcdd05de782a3265e8305162943
Adwait Shende <adwaitshende5656@gmail.com>
2024-06-13 19:23:09 +0530

    fix bugs

--- script.js
+++ script.js
@@ -2,23 +2,26 @@ const list_input = document.getElementById("list_input")
 const submit_button = document.getElementById("submit")
 const list = document.querySelector(".list-container")
 const clear_button = document.getElementById("Clear")
-let items = document.querySelectorAll(".item")
+let items = document.querySelectorAll(".use_flex")
 let labels = document.querySelectorAll(".label")
+let bin = document.querySelectorAll("i")
 let id = 0;
 
 submit_button.addEventListener("click", (e) => {
     let to_add = list_input.value
     if (to_add != "") {
         if (to_add.trim().length) {
-            list.innerHTML += `<input type="checkbox" class="item" id="${id}">
-            <label for="${id}" class="label">${to_add}</label><br>`
+            list.innerHTML += `<div class="use_flex"><div><input type="checkbox" class="item" id="${id}">
+            <label for="${id}" class="label">${to_add}</label></div></div><br>`
         }
     }
     localStorage.setItem("content", `${list.innerHTML}`);
     id++
     list_input.value = ""
-    let items = document.querySelectorAll(".item")
-    let labels = document.querySelectorAll(".label")
+    items = document.querySelectorAll(".use_flex")
+    labels = document.querySelectorAll(".label")
+    bin = document.querySelectorAll("i")
+    console.log(items)
 })
 
 window.addEventListener("load", () => {
