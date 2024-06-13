ceea9db1eed3b5dab809dcc4502a4aec7c13ff12
Adwait Shende <adwaitshende5656@gmail.com>
2024-06-13 18:40:39 +0530

    Make responsive

--- script.js
+++ script.js
@@ -2,23 +2,29 @@ const list_input = document.getElementById("list_input")
 const submit_button = document.getElementById("submit")
 const list = document.querySelector(".list-container")
 const clear_button = document.getElementById("Clear")
+let items = document.querySelectorAll(".item")
+let labels = document.querySelectorAll(".label")
+let id = 0;
 
 submit_button.addEventListener("click", (e) => {
     let to_add = list_input.value
-    let id = 0;
     if (to_add != "") {
         if (to_add.trim().length) {
-            list.innerHTML += `<input type="checkbox" id="${id}">
-            <label for="${id}">${to_add}</label><br>`
+            list.innerHTML += `<input type="checkbox" class="item" id="${id}">
+            <label for="${id}" class="label">${to_add}</label><br>`
         }
     }
     localStorage.setItem("content", `${list.innerHTML}`);
-    console.log(localStorage.getItem("content"))
     id++
+    list_input.value = ""
+    let items = document.querySelectorAll(".item")
+    let labels = document.querySelectorAll(".label")
+    console.log(items)
+    console.log(labels)
+    console.log(id)
 })
 
 window.addEventListener("load", () => {
-    console.log(localStorage.getItem("content"))
     list.innerHTML = localStorage.getItem("content")
 })
 
@@ -26,4 +32,3 @@ clear_button.addEventListener("click", (e) => {
     localStorage.setItem("content", "")
     list.innerHTML = ""
 })
-
