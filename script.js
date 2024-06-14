0cd590c1a16da80280af6000edd99e34c8a2ab50
Adwait Shende <adwaitshende5656@gmail.com>
2024-06-14 14:09:30 +0530

    Add delete feature

--- script.js
+++ script.js
@@ -2,26 +2,20 @@ const list_input = document.getElementById("list_input")
 const submit_button = document.getElementById("submit")
 const list = document.querySelector(".list-container")
 const clear_button = document.getElementById("Clear")
-let items = document.querySelectorAll(".use_flex")
-let labels = document.querySelectorAll(".label")
-let bin = document.querySelectorAll("i")
 let id = 0;
 
 submit_button.addEventListener("click", (e) => {
+    e.preventDefault()
     let to_add = list_input.value
     if (to_add != "") {
         if (to_add.trim().length) {
-            list.innerHTML += `<input type="checkbox" class="item" id="${id}">
-            <label for="${id}" class="label">${to_add}</label><br>`
+            list.innerHTML += `<div class="use_flex"><div><input type="checkbox" class="item" id="${id}">
+            <label for="${id}" class="label">${to_add}</label></div><i class="ri-delete-bin-line"></i></div>`
         }
     }
     localStorage.setItem("content", `${list.innerHTML}`);
     id++
     list_input.value = ""
-    items = document.querySelectorAll(".use_flex")
-    labels = document.querySelectorAll(".label")
-    bin = document.querySelectorAll("i")
-    console.log(items)
 })
 
 window.addEventListener("load", () => {
@@ -31,4 +25,10 @@ window.addEventListener("load", () => {
 clear_button.addEventListener("click", (e) => {
     localStorage.setItem("content", "")
     list.innerHTML = ""
+})
+
+list.addEventListener("click", (event) => {
+    if (event.target.tagName === "I") {
+        event.target.parentElement.remove()
+    }
 })
